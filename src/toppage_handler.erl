177a3fce4c2f8888acad9ef7969ed1befacb3ea6
Daniel Kendell <daniel@kendell.org>
2015-05-14 01:31:11 +0100

    list all dice and their nodes

--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -7,19 +7,21 @@
 
 init(Req, Opts) ->
   { ok, Die } = resource_discovery:get_resource( die ),
+  DiceNodesTxt = [ io_lib:format( "~p ~s~n", [ P, N ] ) || { P, N } <- erd_dice_app:dice_nodes() ],
+
   N = erd_dice_die:roll( Die ),
-	Req2 = cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain">>}
-	], io_lib:format(
-    "Path ~s~n"
+  BodyTxt = io_lib:format(
     "Request handler was ~p running on node ~s~n"
     "Die roll was ~p~n"
     "Die was ~p running on node ~p~n", [
-    cowboy_req:path(Req),
     self(),
     node( self() ),
     N,
     Die,
     node( Die )
-  ] ), Req),
+  ] ),
+
+	Req2 = cowboy_req:reply(200, [
+		{<<"content-type">>, <<"text/plain">>}
+	], [ DiceNodesTxt, "\n\n", BodyTxt ], Req),
 	{ok, Req2, Opts}.
